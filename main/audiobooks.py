95f8fd430a2ddc8648647a04907b125f645665e5
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-19 12:24:46 -0400

    Main Script
    This is the main script used for converting pdf files into audio.

---
+++ main/audiobooks.py
@@ -0,0 +1,22 @@
+import pyttsx3
+import PyPDF2
+
+try:
+    book = open("allergies-in-horses.pdf","rb")
+    pdf_reader = PyPDF2.PdfFileReader(book)
+    pages = pdf_reader.numPages
+
+    print("File successfully uploaded.\n")
+    print(f"This file has {pages} pages.\n")
+    print("Transcripting...\n")
+
+    for i in range(pages):
+        audio = pyttsx3.init()
+        #to_audio = pdf_reader.getPage(0)
+        #text = page.extractText()
+        audio.runAndWait()
+
+    print("Done.")
+except:
+    print("Something went wrong.\n")
+    print("Your file couldn't be transcripted to audio.")
